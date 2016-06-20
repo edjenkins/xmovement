e964d4ce4da38577977d11d84f08a8c2310900ff
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-06-21 00:21:28 +0100

    Show progress in idea tile

--- resources/views/ideas/tile.blade.php
+++ resources/views/ideas/tile.blade.php
@@ -5,12 +5,10 @@
 		    {{ str_limit($idea->name, $limit = 50, $end = '...') }}
 		</a>
 		<p class="idea-description">
-			{{ str_limit($idea->description, $limit = 100, $end = '...') }}
+			{{ str_limit($idea->description, $limit = 150, $end = '...') }}
 		</p>
 	</div>
 	<div class="tile-footer">
-		<p>
-		    Posted by <a href="{{ action('UserController@profile', [$idea->user]) }}">{{ $idea->user->name or $idea->user_id }}</a>
-		</p>
+		@include('ideas/progress')
 	</div>
 </div>
