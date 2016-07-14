43b56ed762aeb56fcc48aabfab6352eb2a1e9943
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-07-14 12:23:25 +0100

    Only show controls to authenticated users

--- resources/views/discussion/comment.blade.php
+++ resources/views/discussion/comment.blade.php
@@ -18,59 +18,65 @@
 			• {{ $comment->created_at->diffForHumans() }}
 		</span>
 	</p>
+
 	<p class="comment-body">
 		{!! nl2br($comment->text) !!}
 	</p>
-	<div class="comment-footer">
 
-		<div class="comment-vote-container {{ ($comment->voteCount() == 0) ? '' : (($comment->voteCount() > 0) ? 'positive-vote' : 'negative-vote') }}">
-			<div class="vote-controls">
-				<div class="vote-button vote-up {{ ($comment->userVote() > 0) ? 'voted' : '' }}" data-vote-direction="up" data-votable-type="comment" data-votable-id="{{ $comment['id'] }}">
-					<i class="fa fa-2x fa-angle-up"></i>
-				</div>
-				<div class="vote-count">
-					{{ $comment->voteCount() }}
-				</div>
-				<div class="vote-button vote-down {{ ($comment->userVote() < 0) ? 'voted' : '' }}" data-vote-direction="down" data-votable-type="comment" data-votable-id="{{ $comment['id'] }}">
-					<i class="fa fa-2x fa-angle-down"></i>
+	@if($authenticated_user)
+
+		<div class="comment-footer">
+
+			<div class="comment-vote-container {{ ($comment->voteCount() == 0) ? '' : (($comment->voteCount() > 0) ? 'positive-vote' : 'negative-vote') }}">
+				<div class="vote-controls">
+					<div class="vote-button vote-up {{ ($comment->userVote() > 0) ? 'voted' : '' }}" data-vote-direction="up" data-votable-type="comment" data-votable-id="{{ $comment['id'] }}">
+						<i class="fa fa-2x fa-angle-up"></i>
+					</div>
+					<div class="vote-count">
+						{{ $comment->voteCount() }}
+					</div>
+					<div class="vote-button vote-down {{ ($comment->userVote() < 0) ? 'voted' : '' }}" data-vote-direction="down" data-votable-type="comment" data-votable-id="{{ $comment['id'] }}">
+						<i class="fa fa-2x fa-angle-down"></i>
+					</div>
 				</div>
 			</div>
+
+			<span class="comment-action-button reply-to-comment-button">Reply</span>
+
+			@if(Gate::forUser($authenticated_user)->allows('destroy', $comment))
+
+				<span class="comment-action-button destroy-comment-button" data-comment-id="{{ $comment->id }}" data-delete-confirmation="{{ trans('idea.comment_delete_confirmation') }}">Delete</span>
+
+			@endif
+
+			@if(Gate::forUser($authenticated_user)->allows('report', $comment))
+
+				<span class="comment-action-button report-comment-button" data-comment-id="{{ $comment->id }}" data-report-confirmation="{{ trans('idea.comment_report_confirmation') }}">
+					<span class="idle-state">
+						Report
+					</span>
+					<span class="reported-state">
+						You reported this
+					</span>
+				</span>
+
+			@endif
+
+			<div class="clearfloat"></div>
+
 		</div>
 
-		<span class="comment-action-button reply-to-comment-button">Reply</span>
+		<div class="post-reply-container">
 
+			@include('discussion.comment-composer', ['authenticated_user' => $authenticated_user, 'comment' => $comment])
 
-		@if(Gate::forUser($authenticated_user)->allows('destroy', $comment))
+		</div>
 
-			<span class="comment-action-button destroy-comment-button" data-comment-id="{{ $comment->id }}" data-delete-confirmation="{{ trans('idea.comment_delete_confirmation') }}">Delete</span>
-
-		@endif
-
-		@if(Gate::forUser($authenticated_user)->allows('report', $comment))
-
-			<span class="comment-action-button report-comment-button" data-comment-id="{{ $comment->id }}" data-report-confirmation="{{ trans('idea.comment_report_confirmation') }}">
-				<span class="idle-state">
-					Report
-				</span>
-				<span class="reported-state">
-					You reported this
-				</span>
-			</span>
-
-		@endif
-
-		<div class="clearfloat"></div>
-
-	</div>
-	<div class="post-reply-container">
-
-		@include('discussion.comment-composer', ['authenticated_user' => $authenticated_user, 'comment' => $comment])
-
-	</div>
+	@endif
 
 	<ul class="comment-replies">
 		@foreach($comment->children as $reply)
-			@include('discussion.comment', ['comment' => $reply, 'authenticated_user_id' => Auth::user()->id])
+			@include('discussion.comment', ['comment' => $reply, 'authenticated_user' => $authenticated_user])
 		@endforeach
 	</ul>
 
