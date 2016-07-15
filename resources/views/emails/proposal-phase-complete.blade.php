6d4b2a85261e02b6244e1136d7ba662cb958ab0e
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-07-15 16:49:15 +0100

    Updated email text

--- resources/views/emails/proposal-phase-complete.blade.php
+++ resources/views/emails/proposal-phase-complete.blade.php
@@ -9,13 +9,13 @@
 	</p>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
-		The most popular blueprint was:
+		The most popular plan was:
 	</p>
 
 	@include('emails/proposal-tile', ['proposal' => $proposal])
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
-		 <a style="color: white; background-color: #6CCCA4; padding: 10px 20px; border-radius: 6px; text-decoration: none; font-weight: normal; font-size: 1.1em;" href="{{ action('ProposeController@index', $idea) }}">Browse other blueprints</a>
+		 <a style="color: white; background-color: #6CCCA4; padding: 10px 20px; border-radius: 6px; text-decoration: none; font-weight: normal; font-size: 1.1em;" href="{{ action('ProposeController@index', $idea) }}">Browse other plans</a>
 	</p>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
@@ -23,7 +23,7 @@
 	</p>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
-		The Support, Get Involved and Blueprint phases are all closed, so now it’s time to make the idea into a reality. The <a href="{{ action('IdeaController@view', $idea) }}">discussion</a> is still open, so check it out for more information…
+		The Support, Get Involved and plan phases are all closed, so now it’s time to make the idea into a reality. The <a href="{{ action('IdeaController@view', $idea) }}">discussion</a> is still open, so check it out for more information…
 	</p>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
--- resources/views/emails/support.blade.php
+++ resources/views/emails/support.blade.php
@@ -5,7 +5,7 @@
 	<h3>Thanks for supporting {{ $idea->name }}, so what’s next?</h3>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
-		Ideas go through different phases, and you should get involved in each step to help the {{ trans_choice('common.idea', 1) }} become a reality. In the Get Involved phase you can create talking points to help develop and shape the {{ trans_choice('common.idea', 1) }}. Afterwards, in the Blueprint phase, you can help choose what the final event should look like. Check the {{ trans_choice('common.idea', 1) }} page to see what phase it is in.
+		Ideas go through different phases, and you should get involved in each step to help the {{ trans_choice('common.idea', 1) }} become a reality. In the Get Involved phase you can create talking points to help develop and shape the {{ trans_choice('common.idea', 1) }}. Afterwards, in the Plan It phase, you can help choose what the final event should look like. Check the {{ trans_choice('common.idea', 1) }} page to see what phase it is in.
 	</p>
 
 	<p style="font-family: 'Segoe UI Light', 'Segoe UI Web Light', 'Segoe UI Web Regular', 'Segoe UI', 'Segoe UI Symbol', HelveticaNeue-Light, 'Helvetica Neue', Arial, sans-serif; font-size: 1.2em; font-weight: 200; margin: 30px 0; padding: 0;">
