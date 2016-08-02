8ff3cb4c1e74ca551a25ae9059493dab3133ce38
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-08-02 13:18:00 +0100

    Added cookie consent plugin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,14 @@
     {!! MetaTag::twitterCard() !!}
 
     {!! MetaTag::tag('image',  getenv('APP_HOME_HEADER_IMG')) !!}
+	<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+	<script type="text/javascript">
+	    window.cookieconsent_options = {"message":"We use cookies to ensure you get the best experience on our website","dismiss":"Got It","learnMore":"","link":null,"theme":"light-floating"};
+	</script>
+
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+	<!-- End Cookie Consent plugin -->
+
 
 </head>
 <body class="fade-nav {{ $bodyclasses or '' }}" id="app-layout" ng-app="XMovement">
