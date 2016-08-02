29330e2d35d263b38b872529e1bf23f64734da53
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-08-02 13:59:05 +0100

    Updated .gitignore

--- resources/views/xmovement/requirement/emails/invite.blade.php
+++ resources/views/xmovement/requirement/emails/invite.blade.php
@@ -10,6 +10,8 @@
 		{{ $user['name'] }} added the following message:
 	<br /><br />
 		"{{ $personal_message }}"
+	<br /><br />
+		Have fun, XMovement team
 	</p>
 
 	@include('../../../emails/email-footer')
