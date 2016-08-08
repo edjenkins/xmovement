7e7526be99295b8583d36d75c2d2af37c44d30f2
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-08-08 14:46:08 +0100

    Updated dependency loading

--- gulpfile.js
+++ gulpfile.js
@@ -17,15 +17,15 @@ var jscookie = require('js-cookie');
 elixir(function(mix) {
 
 	mix.scripts([
-		'bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.js',
-		'bower_components/js-cookie/src/js.cookie.js'
+		'./bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.js',
+		'./bower_components/js-cookie/src/js.cookie.js'
 	], 'public/js/vendor.js');
+
+	mix.copy('./bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.css', 'public/css/jquery-ui-slider-pips.css');
 
 	mix.scriptsIn('resources/assets/js/angular', 'public/js/angular.js');
 
 	mix.copy('resources/assets/css/*', 'public/css');
-
-	mix.copy('resources/assets/js/bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.css', 'public/css/jquery-ui-slider-pips.css');
 
 	mix.stylus('app.styl', null, { use: [ bootstrap() ] });
 
