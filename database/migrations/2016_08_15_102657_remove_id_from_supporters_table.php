0c4f395f3c6301a277bb7daa312f8fa72a51cb21
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-08-15 11:53:23 +0100

    Remove id from supporters pivot table and set composite key to prevent duplicate support records

---
+++ database/migrations/2016_08_15_102657_remove_id_from_supporters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveIdFromSupportersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('supporters', function ($table) {
+            $table->dropColumn('id');
+			$table->primary(['user_id', 'idea_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('supporters', function ($table) {
+            $table->increments('id');
+        });
+    }
+}
