cae2d20855fddba9f9e45e3a1376daa41894a201
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-11-02 11:28:42 +0000

    Updated error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,13 @@
-@extends('layouts.error')
+@extends('layouts.error', ['bodyclasses' => 'colorful'])
 
 @section('content')
 
     <div class="container-fluid error-page">
 
+		<div class="logo-container">
+			<img class="logo-white" src="{{ asset(env('S3_URL') . '/logos/logo-white.png') }}" alt="{{ trans('common.brand') }}" />
+		</div>
+		
         <div class="text-container">
             <h1>{{ trans('errors.404') }}</h1>
 			<h3>{{ trans('errors.page_not_found') }}</h3>
