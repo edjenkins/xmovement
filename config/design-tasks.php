02e09ec6b0f45e421623a84f80925b4174277aca
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-11-07 10:55:00 -0500

    Updated design tasks

--- config/design-tasks.php
+++ config/design-tasks.php
@@ -4,16 +4,16 @@ return array(
 
     'discussions' => [
         [
-			'name' => 'Discussion A',
-	        'description' => 'Description of discussion A'
+			'name' => 'Time',
+	        'description' => 'When would be the best time to hold this event? (Time of day, day of the week, month/semester)'
 		],
         [
-			'name' => 'Discussion B',
-	        'description' => 'Description of discussion B'
+			'name' => 'Capacity',
+	        'description' => 'How many people can attend this event? (The Library has rooms suitable for 2-40 people, other buildings on campus can hold more)'
 		],
         [
-			'name' => 'Discussion C',
-	        'description' => 'Description of discussion C'
+			'name' => 'Event Facilitator',
+	        'description' => 'Who would be the best person to lead/facilitate this event? (Student, Library staff, other University staff, external)'
 		],
     ]
 
