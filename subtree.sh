810dae51aebbe54caa4d6d3d8606742607c2387d
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-11-09 12:00:40 +0000

    Added subtree.sh

---
+++ subtree.sh
@@ -0,0 +1,3 @@
+git remote add deployment ssh://git@openlab.ncl.ac.uk:17150/edjenkins/xmovement-deployments.git
+git subtree add --prefix=packages/deployment/ deployment library
+#git subtree pull --prefix=packages/deployment/ deployment library
