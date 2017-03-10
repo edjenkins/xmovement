d1198e8c18a6dacbaeffffb972618eca0bb9e4ee
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-10 17:03:08 +0000

    Updated outdated component

--- resources/views/components/outdated.blade.php
+++ resources/views/components/outdated.blade.php
@@ -5,11 +5,27 @@
 </div>
 
 <script type="text/javascript">
-	//DOM ready or jQuery
-	outdatedBrowser({
-		bgColor: '#f25648',
-		color: '#ffffff',
-		lowerThan: 'transform',
-		languagePath: ''
-	})
+	//event listener: DOM ready
+	function addLoadEvent(func) {
+		var oldonload = window.onload;
+		if (typeof window.onload != 'function') {
+			window.onload = func;
+		} else {
+			window.onload = function() {
+				if (oldonload) {
+					oldonload();
+				}
+				func();
+			}
+		}
+	}
+	//call plugin function after DOM ready
+	addLoadEvent(function(){
+		outdatedBrowser({
+			bgColor: '#f25648',
+			color: '#ffffff',
+			lowerThan: 'transform',
+			languagePath: 'your_path/outdatedbrowser/lang/en.html'
+		})
+	});
 </script>
