4e40f06cdc2967fa6543541809792174ff4b450b
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-04-23 12:10:44 +0100

    Updates

--- app/Http/Middleware/SetReferer.php
+++ app/Http/Middleware/SetReferer.php
@@ -20,22 +20,20 @@ class SetReferer
      */
     public function handle($request, Closure $next)
     {
-		# DEFAULT META
-		MetaTag::set('title', Lang::get('meta.default_title'));
-		MetaTag::set('description', Lang::get('meta.default_description'));
-		MetaTag::set('image', env('APP_SITE_IMAGE'));
-		# DEFAULT META
+        # DEFAULT META
+        MetaTag::set('title', Lang::get('meta.default_title'));
+        MetaTag::set('description', Lang::get('meta.default_description'));
+        MetaTag::set('image', env('APP_SITE_IMAGE'));
+        # DEFAULT META
 
 
-		if (!((strpos(URL::previous(), 'login') || strpos(URL::previous(), 'register') || strpos(URL::previous(), 'auth'))))
-		{
-			if (strpos(URL::previous(), $_SERVER['HTTP_HOST']))
-			{
-				Log::info('Setting redirect to - ' . URL::previous());
-				Session::set('redirect', URL::previous());
-			}
-		}
+        if (!((strpos(URL::previous(), 'login') || strpos(URL::previous(), 'register') || strpos(URL::previous(), 'auth')))) {
+            if (strpos(URL::previous(), $_SERVER['HTTP_HOST'])) {
+                Log::info('Setting redirect to - ' . URL::previous());
+                Session::set('redirect', URL::previous());
+            }
+        }
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 }
